57b1837d3eb5104b9dee3aa6d547394a31531537
Nicholas Licata <nicholas.licata730@gmail.com>
2023-10-13 23:11:06 -0400

    added state for navActive and resize to close nav above 768px

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,30 @@
+import { useEffect, useState } from 'react';
+import Home from './components/Home';
 
 function App() {
+  const [navActive, setNavActive] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener('resize', function() {
+      if (window.innerWidth > 768 && navActive) {
+        closeNav();
+      }
+    })
+  })
+
+  function toggleNav() {
+    setNavActive(!navActive);
+  }
+
+  function closeNav() {
+    setNavActive(false);
+  }
 
   return (
-    <>
-    </>
+    <main>
+      <Home navActive={navActive} toggleNav={toggleNav}/>
+    </main>
   )
 }
 
-export default App
+export default App;
