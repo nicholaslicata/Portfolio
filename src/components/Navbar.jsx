46a243d8e1ddd26d56514d4ee6c22d22902618fe
Nicholas Licata <nicholas.licata730@gmail.com>
2023-10-19 00:38:33 -0400

    updated nav logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ function Navbar({ navActive, toggleNav }) {
 
     return (
       <header className='navbar-container'>
-          <a href='#home' className='navbar-logo'>Nicholas Licata</a>
+          <a href='#home' className='navbar-logo'>NL</a>
           <nav className={navActive ? 'nav-active' : 'nav'}>
             <a className='nav-link' href='#home'>Home</a>
             <a className='nav-link' href='#about'>About</a>
