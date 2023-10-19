d0a1716eb4523d71be166379dea58e9dd802a961
Nicholas Licata <nicholas.licata730@gmail.com>
2023-10-19 00:39:03 -0400

    updated Home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -9,8 +9,8 @@ function Home() {
                 <p className='home-intro-text'>Greetings! My name is...</p>
                 <h1 className='home-intro-name'>Nicholas Licata</h1>
                 <span className='home-intro-subtext'>Front End Developer</span>
-                <p className='home-intro-p'>A front end web developer who focuses on creating effective
-                    and functional web experiences. Here's a little bit more <a className='home-intro-about-link' href='#'>about me..</a>
+                <p className='home-intro-p'>A front end web developer who focuses on creating functional
+                    and effective web experiences. Here's a little bit more <a className='home-intro-about-link' href='#'>about me..</a>
                 </p>
                 <div className='home-intro-buttons'>
                 <button className='home-intro-resume'>View Resume</button>
