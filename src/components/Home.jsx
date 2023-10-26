63d326e1044cd69d172f5b898268097a96a069ac
Nicholas Licata <nicholas.licata730@gmail.com>
2023-10-25 23:48:27 -0400

    updated home component

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,14 +3,14 @@ import { FaGithub, FaLinkedin } from 'react-icons/fa';
 function Home() {
 
     return (
-        <section className='home'>
+        <section className='home' id='home'>
             <div className='home-intro-container'>
               <div className='home-text-container'>
                 <p className='home-intro-text'>Greetings! My name is...</p>
                 <h1 className='home-intro-name'>Nicholas Licata</h1>
                 <span className='home-intro-subtext'>Front End Developer</span>
                 <p className='home-intro-p'>A front end web developer who focuses on creating functional
-                    and effective web experiences. Here's a little bit more <a className='home-intro-about-link' href='#'>about me..</a>
+                    and effective web experiences. Here's a little bit more <a className='home-intro-about-link' href='#about'>about me..</a>
                 </p>
                 <div className='home-intro-buttons'>
                 <button className='home-intro-resume'>View Resume</button>
