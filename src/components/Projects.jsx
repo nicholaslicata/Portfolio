9f2a6bce7ca120f744f65b6f76187f16849d5ae1
Nicholas Licata <nicholas.licata730@gmail.com>
2023-11-02 23:55:03 -0400

    setup basic component layout

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,8 +1,20 @@
+import projectList from '../projectList';
+import Project from '../components/Project';
 
 function Projects() {
+
     return (
-        <section>
-            <p>Projects</p>
+        <section className='projects' id='projects'>
+            <div className='projects-title-container'>
+                <h3 className='projects-title'>Projects</h3>
+            </div>
+            <div className='projects-list-container'>
+            {projectList.map((projectItem) => (
+                    <Project projectItem={projectItem} />
+            ))}
+            </div>
         </section>
     )
 }
+
+export default Projects;
