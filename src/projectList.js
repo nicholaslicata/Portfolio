5230a4bcaf369553440d24ac461e90810d6237b4
Nicholas Licata <nicholas.licata730@gmail.com>
2023-11-02 23:55:22 -0400

    initial file upload

---
+++ src/projectList.js
@@ -0,0 +1,50 @@
+import BurgerBarnImg from './assets/burger-barn.png';
+import EldenRingImg from './assets/elden-ring-compendium.png';
+import SolarSystemImg from './assets/the-solar-system.png';
+import TVForMeImg from './assets/tv-for-me.png';
+import JourneyJotterImg from './assets/journey-jotter.png';
+
+  const projectList = [
+    {
+        title: 'Burger Barn',
+        tech: ['HTML', 'CSS', 'Javascript'],
+        description: 'A website for the fictional restaurant Burger Barn',
+        demoLink: 'https://nicholaslicata.github.io/Burger-Barn-Restaurant/',
+        githubLink: 'https://github.com/nicholaslicata/Burger-Barn-Restaurant',
+        img: BurgerBarnImg
+    },
+    {
+        title: 'Elden Ring Compendium',
+        tech: ['HTML', 'CSS', 'Javascript', 'Elden Ring Fan API'],
+        description: 'This project compiles data for the video game Elden Ring. The user can search through various categories related to the game.',
+        demoLink : 'https://nicholaslicata.github.io/Elden-Ring-Compendium/',
+        githubLink: 'https://github.com/nicholaslicata/Elden-Ring-Compendium',
+        img: EldenRingImg
+    },
+    {
+        title: 'The Solar System',
+        tech: ['React', 'CSS'],
+        description: 'An educational website that provides the user with interesting facts about all of the planets in the Solar System.',
+        demoLink: 'https://nicholaslicata.github.io/The-Solar-System/',
+        githubLink: 'https://github.com/nicholaslicata/The-Solar-System',
+        img: SolarSystemImg,
+    },
+    {
+        title: 'TV For Me',
+        tech: ['React', 'CSS', 'TV Maze API'],
+        description: 'This project allows users to find information about TV shows or their favorite actor/actress. They also have access to a watchlist to keep track of their favorite shows.',
+        demoLink: 'https://nicholaslicata.github.io/TV-For-Me/',
+        githubLink: 'https://github.com/nicholaslicata/TV-For-Me',
+        img: TVForMeImg
+    },
+    {
+        title: 'Journey Jotter',
+        tech: ['React', 'Firebase', 'CSS'],
+        description: 'With Journey Jotter the user can track all of the bucket list items that they wish to achieve in their journey through life.',
+        demoLink: 'https://journey-jotter.web.app/',
+        githubLink: 'https://github.com/nicholaslicata/journey-jotter',
+        img: JourneyJotterImg
+    }
+]
+
+export default projectList;
