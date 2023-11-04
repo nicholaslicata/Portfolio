841da90541df53591fde889d21df7cf1ada5cd21
Nicholas Licata <nicholas.licata730@gmail.com>
2023-11-03 22:49:54 -0400

    updated projectList array order

--- src/projectList.js
+++ src/projectList.js
@@ -6,28 +6,12 @@ import JourneyJotterImg from './assets/journey-jotter.png';
 
   const projectList = [
     {
-        title: 'Burger Barn',
-        tech: ['HTML', 'CSS', 'Javascript'],
-        description: 'A website for the fictional restaurant Burger Barn',
-        demoLink: 'https://nicholaslicata.github.io/Burger-Barn-Restaurant/',
-        githubLink: 'https://github.com/nicholaslicata/Burger-Barn-Restaurant',
-        img: BurgerBarnImg
-    },
-    {
-        title: 'Elden Ring Compendium',
-        tech: ['HTML', 'CSS', 'Javascript', 'Elden Ring Fan API'],
-        description: 'This project compiles data for the video game Elden Ring. The user can search through various categories related to the game.',
-        demoLink : 'https://nicholaslicata.github.io/Elden-Ring-Compendium/',
-        githubLink: 'https://github.com/nicholaslicata/Elden-Ring-Compendium',
-        img: EldenRingImg
-    },
-    {
-        title: 'The Solar System',
-        tech: ['React', 'CSS'],
-        description: 'An educational website that provides the user with interesting facts about all of the planets in the Solar System.',
-        demoLink: 'https://nicholaslicata.github.io/The-Solar-System/',
-        githubLink: 'https://github.com/nicholaslicata/The-Solar-System',
-        img: SolarSystemImg,
+        title: 'Journey Jotter',
+        tech: ['React', 'Firebase', 'CSS'],
+        description: 'With Journey Jotter the user can track all of the bucket list items that they wish to achieve in their journey through life.',
+        demoLink: 'https://journey-jotter.web.app/',
+        githubLink: 'https://github.com/nicholaslicata/journey-jotter',
+        img: JourneyJotterImg
     },
     {
         title: 'TV For Me',
@@ -38,12 +22,28 @@ import JourneyJotterImg from './assets/journey-jotter.png';
         img: TVForMeImg
     },
     {
-        title: 'Journey Jotter',
-        tech: ['React', 'Firebase', 'CSS'],
-        description: 'With Journey Jotter the user can track all of the bucket list items that they wish to achieve in their journey through life.',
-        demoLink: 'https://journey-jotter.web.app/',
-        githubLink: 'https://github.com/nicholaslicata/journey-jotter',
-        img: JourneyJotterImg
+        title: 'The Solar System',
+        tech: ['React', 'CSS'],
+        description: 'An educational website that provides the user with interesting facts about all of the planets in the Solar System.',
+        demoLink: 'https://nicholaslicata.github.io/The-Solar-System/',
+        githubLink: 'https://github.com/nicholaslicata/The-Solar-System',
+        img: SolarSystemImg,
+    },
+    {
+        title: 'Elden Ring Compendium',
+        tech: ['HTML', 'CSS', 'Javascript', 'Elden Ring Fan API'],
+        description: 'This project compiles data for the video game Elden Ring. The user can search through various categories related to the game.',
+        demoLink : 'https://nicholaslicata.github.io/Elden-Ring-Compendium/',
+        githubLink: 'https://github.com/nicholaslicata/Elden-Ring-Compendium',
+        img: EldenRingImg
+    },
+    {
+        title: 'Burger Barn',
+        tech: ['HTML', 'CSS', 'Javascript'],
+        description: 'A website for the fictional restaurant Burger Barn',
+        demoLink: 'https://nicholaslicata.github.io/Burger-Barn-Restaurant/',
+        githubLink: 'https://github.com/nicholaslicata/Burger-Barn-Restaurant',
+        img: BurgerBarnImg
     }
 ]
 
