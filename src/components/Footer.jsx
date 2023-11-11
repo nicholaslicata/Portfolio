ad21a5b8d137a86b5911f8a4d0bbfa6f14aa8679
Nicholas Licata <nicholas.licata730@gmail.com>
2023-11-11 14:47:51 -0500

    added aria label to link icons

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,8 +6,8 @@ function Footer() {
             <div className='footer-content-container'>
                 <a className='footer-email' href='mailto:nicholas.licata730@gmail.com'>nicholas.licata730@gmail.com</a>
                 <div className='footer-icons-container'>
-                  <a href='https://github.com/nicholaslicata' target='_blank'><FaGithub className='footer-icon' /></a>
-                  <a href='https://linkedin.com/in/nicholaslicata' target='_blank'><FaLinkedin className='footer-icon' /></a>
+                  <a href='https://github.com/nicholaslicata' target='_blank' aria-label='Visit my Github'><FaGithub className='footer-icon' /></a>
+                  <a href='https://linkedin.com/in/nicholaslicata' target='_blank' aria-label='Visit my LinkedIn'><FaLinkedin className='footer-icon' /></a>
                 </div>
             </div>
         </footer>
