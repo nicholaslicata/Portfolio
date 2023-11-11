ec60d933965735d2fcfb5a298a29d6850ac27906
Nicholas Licata <nicholas.licata730@gmail.com>
2023-11-10 23:04:17 -0500

    rendered footer component

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Home from './components/Home';
 import About from'./components/About';
 import Projects from './components/Projects';
 import Contact from './components/Contact';
+import Footer from './components/Footer';
 
 function App() {
   const [navActive, setNavActive] = useState(false);
@@ -37,6 +38,7 @@ function App() {
       <About />
       <Projects />
       <Contact />
+      <Footer />
     </main>
   )
 }
